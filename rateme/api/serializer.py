cbeefa268e0b2848d8c3111bd95a3a5657c1027d
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-31 17:13:25 +0300

    create a class for the profilr serializer

--- rateme/api/serializer.py
+++ rateme/api/serializer.py
@@ -2,3 +2,7 @@ from rest_framework import serializers
 from rest_framework.views import APIView
 from awwardsy.models import Profile
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id' 'username', 'profile_photo', 'bio', 'contact')
