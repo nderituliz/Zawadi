afb8e9d735afcddc79452d1eb432e729e3513189
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-31 13:36:28 +0300

    created a class for the profile forms

--- rateme/forms.py
+++ rateme/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Profile, Projects, Categories
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["profile_photo", "bio", "contact"]
