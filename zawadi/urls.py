2eed8dcaf6a64eae6771996d66c231878f2d7422
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-31 17:04:39 +0300

    create url patterns

--- zawadi/urls.py
+++ zawadi/urls.py
@@ -5,3 +5,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth import views
 from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('zawadi.urls')),
+    path('accounts/', include('zawadi.urls')),
+    url('accounts/', include('django.contrib.auth.urls')),
+    # url(r'^logout/$', views.logout, {"next_page": '/accounts/login/'}),
+    url(r'^api/', include('zawadi.api.urls')),
+]
