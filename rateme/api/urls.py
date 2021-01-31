4fdc55a21d5b1a5bd9d99ba5c5a66b3ec63f5897
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-31 17:16:22 +0300

    create url paths for the api

--- rateme/api/urls.py
+++ rateme/api/urls.py
@@ -1,3 +1,10 @@
 from django.conf.urls import url
 from .views import ProfileView,  ProjectView
 from rest_framework import routers
+
+
+urlpatterns = [
+    url(r'^profile', ProfileView, name='profileview'),
+    url(r'^project', ProjectView, name='projectView'),
+
+]
