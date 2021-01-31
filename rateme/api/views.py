b7d7ac9c031438ebe21ec17f13d36bcceedd6db2
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-31 17:18:56 +0300

    create a class for the viewing the projects

--- rateme/api/views.py
+++ rateme/api/views.py
@@ -8,3 +8,7 @@ from rest_framework import viewsets
 class ProfileView(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+class ProjectView(viewsets.ModelViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectSerializer
