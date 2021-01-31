ababa0fac4c771179ff42f8b29732c73c5984cdd
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-31 13:24:03 +0300

    created the admin models

--- rateme/admin.py
+++ rateme/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 from .models import Profile, Projects, Categories
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(Categories)
